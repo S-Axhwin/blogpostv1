e3f0afe836cd29ea1f5a6d8825e45ccfac17f531
S-Axhwin <ashwin.sathiyamoorthy.d@gmail.com>
2024-07-18 13:06:59 +0530

    removed base name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/blogPost/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
